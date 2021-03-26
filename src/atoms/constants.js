b7561804e297db360d75de788639b380fe0303b1
Daniel Warren <warrend555@gmail.com>
2021-03-26 16:36:16 -0400

    updates favicon

--- src/atoms/constants.js
+++ src/atoms/constants.js
@@ -1,9 +1,11 @@
 import { atom } from "recoil";
 import theme from "../theme";
 
+const theTheme = theme;
+
 export default {
   theme: atom({
     key: "theme",
-    default: theme,
+    default: theTheme,
   }),
 };
